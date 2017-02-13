e55d8900bd8409f0ed9ccd5cea4f57016027de27
Cole Allan <cacoleallan@gmail.com>
2017-02-13 11:09:08 -0700

    Set up basic redux. Got over and under twenty one buttons going to the correct routes.

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import App from './App';
+import TwentyOneAndOverBox from './components/TwentyOneAndOverBox';
+import IsOverTwentyOne from './components/IsOverTwentyOne';
+import NotOverTwentyOne from './components/NotOverTwentyOne';
 import './index.css';
 
 ReactDOM.render(
-    <App />,
+    <Router history={browserHistory}>
+        <Route path='/' component={TwentyOneAndOverBox} />
+        <Route path='/home' component={IsOverTwentyOne} />
+        <Route path='/denied' component={NotOverTwentyOne} />
+    </Router>,
     document.getElementById('root')
 );
---
+++ client/src/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+import documentsReducer from './documentsReducer';
+// import activeDocumentIdReducer from './activeDocumentIdReducer';
+
+const rootReducer = combineReducers({
+
+});
+
+export default rootReducer;
