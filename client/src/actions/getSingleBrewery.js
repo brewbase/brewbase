7a7b84eea8d3680a4a28df4de063b87142e0b91f
John Munson <johncmunson91@gmail.com>
2017-02-16 23:10:20 -0700

    Install react geolocation higher order component. Use HOC to wrap the searchbar.

--- client/src/actions/getSingleBrewery.js
+++ client/src/actions/getSingleBrewery.js
@@ -1,7 +1,6 @@
 import startFetchingBreweries from './startFetchingBreweries.js'
 import receiveBreweries from './receiveBreweries.js'
 import breweriesError from './breweriesError.js'
-import states from '../data/states.js'
 import config from '../../config.js'
 
 export default function getSingleBrewery (id) {
--- client/src/containers/ConnectedSearchBar.js
+++ client/src/containers/ConnectedSearchBar.js
@@ -1,9 +1,6 @@
-// ConnectedSearchBar responsible for taking user input, updating the userInput on the state tree, and routing to the results view
-
-import React from 'react';
-import {connect} from 'react-redux';
-import SearchBar from '../components/SearchBar.js';
-import updateUserInput from '../actions/updateUserInput.js';
+import { connect } from 'react-redux'
+import SearchBar from '../components/SearchBar.js'
+import updateUserInput from '../actions/updateUserInput.js'
 
 const mapDispatchToProps = (dispatch) => ({
     handleUserInput: (input) => {
