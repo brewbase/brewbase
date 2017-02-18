96f3cef111677826d2d8a59198d882be9a996eab
Cole Allan <cacoleallan@gmail.com>
2017-02-18 12:33:19 -0700

    Fixed issue with content class in main css throwing off the brewery cards in the mobile view

--- client/src/views/SearchResultsView.js
+++ client/src/views/SearchResultsView.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Breweries from '../containers/Breweries.js';
 import DesktopNavBar from '../components/DesktopNavBar.js'
 import MobileNavBar from '../components/MobileNavBar.js'
+import '../styles/main.css';
 
 const SearchResultsView = (props) => (
+<div className="content">
     <div>
         <DesktopNavBar />
-        <Breweries />
         <MobileNavBar />
+        <Breweries />
+
     </div>
+</div>
 )
 
 export default SearchResultsView;
