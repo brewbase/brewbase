f46da66ca6196f956649b7112686a455d6b34a00
John Munson <johncmunson91@gmail.com>
2017-02-19 21:16:20 -0700

    favorites view now displays favorite breweries and favorite brews

--- client/src/views/FavoritesView.js
+++ client/src/views/FavoritesView.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import DesktopNavBar from '../components/DesktopNavBar.js'
 import MobileNavBar from '../components/MobileNavBar.js'
 import ConnectedFavToggle from '../containers/ConnectedFavToggle.js'
+import FavoriteBreweries from '../containers/FavoriteBreweries.js'
+import FavoriteBrews from '../containers/FavoriteBrews.js'
 
-const FavoritesView = (props) => (
-    <div>
-        <DesktopNavBar />
-        <MobileNavBar />
-        <ConnectedFavToggle />
-    </div>
+class FavoritesView extends React.Component {
+    render() {
+        return (
+            <div>
+                <DesktopNavBar />
+                <MobileNavBar />
+                <ConnectedFavToggle />
+                {this.props.favorites === 'brews' ?
+                (<FavoriteBrews />) :
+                (<FavoriteBreweries />)}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => (
+    {
+        favorites: state.favorites
+    }
 )
+
+FavoritesView = connect(mapStateToProps)(FavoritesView)
 
 export default FavoritesView
