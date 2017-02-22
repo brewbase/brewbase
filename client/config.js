634e6849369e86e33756e3c0d842f9b5ef845567
John Munson <johncmunson91@gmail.com>
2017-02-22 12:19:51 -0700

    created a root config file

--- client/config.js
+++ client/config.js
@@ -1,6 +1,3 @@
 module.exports = {
-    clientID: 'j0ICHuTbfDJXIrcQQBNyse42Ec48ul0E',
-    clientSecret: 'V2v7NOWRS0J1SBCzTkWpECgvUVOfMVflPQVMFfTYJRHgD2fYoXyckgiJTdKhE5AI',
-    key: '1168087dc16a437c599b8e48ca2d7c91',
-    connString: "postgres://cesargarcia:@localhost/assessbox"
+    key: '1168087dc16a437c599b8e48ca2d7c91'
 }
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const config = require('./client/config');
+const config = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const massive = require('massive');
