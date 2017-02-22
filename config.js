7a7dce1663f030eb1e4408d02f4de430d5ae4766
John Munson <johncmunson91@gmail.com>
2017-02-22 00:42:19 -0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    clientID: 'j0ICHuTbfDJXIrcQQBNyse42Ec48ul0E',
-    clientSecret: 'V2v7NOWRS0J1SBCzTkWpECgvUVOfMVflPQVMFfTYJRHgD2fYoXyckgiJTdKhE5AI',
-    connString: 'postgres://johnmunson:@localhost/johnmunson'
-}
