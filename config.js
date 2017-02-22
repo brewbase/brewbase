209a43a1b1462ffef2453a89641946c4fcf0c57f
Cesar <cesarxp2@hotmail.com>
2017-02-22 12:33:10 -0700

    Edited server

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+clientID: 'j0ICHuTbfDJXIrcQQBNyse42Ec48ul0E',
+clientSecret: 'V2v7NOWRS0J1SBCzTkWpECgvUVOfMVflPQVMFfTYJRHgD2fYoXyckgiJTdKhE5AI',
+connString: "postgres://evslxpgg:xcb1RAFUWa6bpUmVpvG9o8h4Ss13LNuX@hard-plum.db.elephantsql.com:5432/evslxpgg"
+}
