6e694966f0a97e1d90ec316a3392e6796281fa94
Nicholas Mastracola <nicholas.mastracola@gmail.com>
2017-02-27 12:41:58 -0700

    styled profile view for desktop

--- client/src/views/ProfileView.js
+++ client/src/views/ProfileView.js
@@ -17,14 +17,18 @@ class ProfileView extends React.Component {
                     :
                     this.props.userData.name ? null : this.props.fetchUserData(this.context.router.location.query.id)
                 }
-                <div className='userDataContainer'>
-                 <img className='userProfilePic' src={this.props.userData.imgUrl}></img>
-                 <h2> {this.props.userData.name} </h2>
-                 <hr></hr>
-                 <p> Artisan migas salvia master cleanse, gluten-free fingerstache skateboard cronut authentic hell of blog paleo mixtape. Selvage kombucha austin, normcore pork belly cronut asymmetrical tousled iPhone pour-over.</p>
+                <div className="profileContainer">
+                    <div className='userDataContainer'>
+                     <img className='userProfilePic' src={this.props.userData.imgUrl} alt="profile"></img>
+                     <h2> {this.props.userData.name} </h2>
+                     <hr></hr>
+                     <p> Artisan migas salvia master cleanse, gluten-free fingerstache skateboard cronut authentic hell of blog paleo mixtape. Selvage kombucha austin, normcore pork belly cronut asymmetrical tousled iPhone pour-over.</p>
+                    </div>
+                    <div className='favoritesDesc'> Favorite Breweries: 12 <br/><br/> Favorite Brews: 4</div>
+                    <div className='logoutButton'>
+                        <a href='//localhost:4000/logout'><button> SIGN OUT </button></a>
+                    </div>
                 </div>
-                <div className='favoritesDesc'> Favorite Breweries: 12 <br/><br/> Favorite Brews: 4</div>
-                <div className='logoutButton'>  <a href='//localhost:4000/logout'><button> SIGN OUT </button></a> </div>
             </div>
         )
     }
