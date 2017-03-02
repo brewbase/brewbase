6f85a97e1a792f48781af5216ea9aa30440e00c3
Nicholas Mastracola <nicholas.mastracola@gmail.com>
2017-03-01 17:05:36 -0700

    finalized new getbeers endpoint and action request

--- client/src/actions/getBrews.js
+++ client/src/actions/getBrews.js
@@ -1,15 +1,17 @@
 import startFetchingBrews from './startFetchingBrews.js'
 import receiveBrews from './receiveBrews.js'
 import brewsError from './brewsError.js'
-import config from '../../config.js'
 
 export default function getBrews(id) {
     return function(dispatch) {
         dispatch(startFetchingBrews())
-        return fetch(`https://api.brewerydb.com/v2/brewery/${id}/beers?key=${config.key}&format=json`)
-        .then(response => response.json())
-        .then(json => {
-            dispatch(receiveBrews(json.data))
+        return axios({ 
+                method: 'post',
+                url: `/brewdb/beers`, 
+                data: {id} 
+        })
+        .then(response => {
+            dispatch(receiveBrews(response.data))
         })
         .catch(err => dispatch(brewsError(err)))
     }
